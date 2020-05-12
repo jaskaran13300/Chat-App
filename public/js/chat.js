46bc39de4a61ce6e7566024edbebee664b198462
jaskaran13300 <42165769+jaskaran13300@users.noreply.github.com>
2020-05-12 22:35:29 +0530

    Render messages to browser using Mustache Library

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,44 @@
 const socket = io();
+const $messageForm=document.querySelector('#message-form');
+const $messageFormInput=$messageForm.querySelector('input');
+const $messageFormButton=$messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages=document.querySelector('#messages');
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+const $messageTemplate=document.querySelector('#message-template').innerHTML;
+
+socket.on('message', (message) => {
+    const html=Mustache.render($messageTemplate,{
+        message
+    });
+    $messages.insertAdjacentHTML('beforeend',html);
+    console.log(message);
+})
+
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    $messageForm.setAttribute('disabled','disabled');
     const message = e.target.elements.inputMessage.value;
     socket.emit('sendMessage',message,(error)=>{
+        $messageForm.removeAttribute('disabled');
+        $messageFormInput.value='';
+        $messageFormInput.focus();
         if(error){
             return console.log(error);
         }
-        console.log(message);
+        console.log("Message Delivered");
     });
 })
-document.querySelector('#send-location').addEventListener('click',()=>{
+$sendLocationButton.addEventListener('click',()=>{
     if(!navigator.geolocation){
         return alert('Your web browser does not support for sending location')
     }
+    $sendLocationButton.setAttribute('disabled','disabled');
     navigator.geolocation.getCurrentPosition((position)=>{
         socket.emit('sendLocation', `https://google.com/maps?q=${position.coords.latitude},${position.coords.longitude}`,(deliveryStatus)=>{
+            $sendLocationButton.removeAttribute('disabled');
             console.log("Location ",deliveryStatus);
         });
     })
-})
-socket.on('message', (message) => {
-    console.log(message);
 })
